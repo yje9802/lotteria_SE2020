651bc220bc71b54a3ec4f4631ebcc5dab0ab094b
joons5834 <hwajunepat@gmail.com>
2020-11-30 17:43:33 +0900

    order Blueprint의 view 템플릿 생성
    html이 kiosk/templates/order 바로 아래에 있다고 가정. 일단 각 웹페이지를 열 수 있을 정도로만 작업.

--- kiosk/order.py
+++ kiosk/order.py
@@ -2,11 +2,34 @@
 # https://flask.palletsprojects.com/en/1.1.x/tutorial/views/ 참고
 # https://flask.palletsprojects.com/en/1.1.x/tutorial/blog/ 참고
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.exceptions import abort
 
-from flaskr.auth import login_required
-from flaskr.db import get_db
+from kiosk.db import get_db
 
-bp = Blueprint('order', __name__)
+bp = Blueprint('order', __name__, url_prefix='/order')
+
+@bp.route('/')
+def index():
+    return render_template('order/home.html')
+    
+
+@bp.route('/payment')
+def payment():
+    return render_template('order/payment.html')
+
+
+@bp.route('/menu')
+def menu():
+    return render_template('order/menu.html')
+    
+
+@bp.route('/charge')
+def charge():
+    return render_template('order/charge.html')
+
+
+@bp.route('/register')
+def register():
+    return 'Successfully registered your order. Your order no is xx.' # 여기를 주문완료 html로 대체
