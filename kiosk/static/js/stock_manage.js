13522fa4513425b1b116f3c7cda4f14efa56216b
boram2445 <68495264+boram2445@users.noreply.github.com>
2020-12-05 17:27:26 +0900

    stock_manage 기능 추가
    1. html 뒤로가기 버튼 생성 (경로 수정 필요)
    2. 메뉴 선택시 품절처리 창 뜨는것 생성 js
    3. 표 수정 html, css
    4. '품절 상태로 변경' 버튼 클릭시 판매 가능으로 변경으로 버튼 변경, 메뉴명 빨간색으로 변경. 다시 클릭하면 원래대로.
    (품절처리 버튼을 눌렀을때 해당 메뉴의 class에 ".need"를 추가하도록 하였는데, 메뉴 선택으로 돌아가면적용되지 않는것 같음. 경로 변경이 필요할것 같고, 해당 메뉴의 class에만 적용되는게 맞는지도 확인해야 할 부분임. 이 부분은 db가 관여해야 되는 부분일지도??)

--- kiosk/static/js/stock_manage.js
+++ kiosk/static/js/stock_manage.js
@@ -28,6 +28,105 @@ function categorySelect(){
     })
 }
 
+//메뉴 클릭시 재고상태 확인 
+function selectMenu(){
+    const container = document.querySelector(".container");
+    menus.forEach((menu)=>{
+        menu.addEventListener("click", function(event){
+            container.classList.add("ingredients");
+            const name = menu.innerHTML;
+            container.innerHTML=`
+            <div class="ingredients_menu_container">
+                <div class="menu_name">${name}</div>
+                <button class="menu_btn">품절 상태로 변경</button>
+            </div>
+            <div class="ingredients_table_container">
+                <table border="1">
+                <th>재고명</th>
+                <th>수량</th>
+                <tr>
+                    <td>빵</td>
+                    <td>100</td>
+                </tr>
+                <tr>
+                    <td>크리스피 치킨 패티</td>
+                    <td>100</td>
+                </tr>
+                <tr>
+                    <td>양상추 슬라이스</td>
+                    <td>67</td>
+                </tr>
+                <tr>
+                    <td class="need">토마토 슬라이스</td>
+                    <td class="need">3</td>
+                </tr>
+                <tr>
+                    <td>머스타드 크림소스</td>
+                    <td>50</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                </tr>
+                </table>
+            </div>
+            <div class="ingredients_discribe">*수량이 5 이하인 경우 빨간색으로 표시됩니다.</div>
+            `;
+
+            turnMenuSoldout(menu);
+        })
+
+        
+    })
+}
+
+//품절 상태 변경 btn 클릭시 메뉴명 빨간색으로
+function turnMenuSoldout(menu){
+    const soldout_btn = document.querySelector(".menu_btn");
+    const menu_name = document.querySelector(".menu_name");
+    const soldout_class = "soldout";
+    soldout_btn.addEventListener("click",()=>{
+        soldout_btn.classList.add(soldout_class);
+        soldout_btn.innerHTML = `판매 가능으로 변경`;
+        menu_name.classList.add("need");
+        menu.classList.add("need");
+        soldout_btn.addEventListener("click", ()=>{
+            hasClicked(menu,soldout_btn, menu_name, soldout_class);
+
+        })
+    })
+}
+
+
+//판매 가능 버튼이 클릭되었는지 확인 
+function hasClicked(menu,soldout_btn, menu_name, soldout_class){
+    const hasClass = soldout_btn.classList.contains(soldout_class);
+    if(hasClass){
+        soldout_btn.classList.remove(soldout_class);
+        soldout_btn.innerHTML = `품절 상태로 변경`;
+        menu_name.classList.remove("need");
+        menu.classList.remove("need");
+    } else{
+        soldout_btn.classList.add(soldout_class);
+        soldout_btn.innerHTML = `판매 가능으로 변경`;
+        menu_name.classList.add("need");
+        menu.classList.add("need");
+    }
+    
+}
+
+
+
+//판매 가능으로 변경
+function turnMenuCanorder(){
+    soldout_btn.onclick = function(){
+        menu_name.classList.remove("need");
+        soldout_btn.innerHTML = `품절 상태로 변경`;
+        menu.classList.remove("need");
+    }
+}
 
 categorySelect();
+selectMenu();
+
 
