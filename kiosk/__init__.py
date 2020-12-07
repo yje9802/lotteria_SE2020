253b20f7f2da71d3e54d36ae6c89485da421475a
joons5834 <hwajunepat@gmail.com>
2020-12-07 11:23:41 +0900

    메뉴추가 모듈 생성
    Co-Authored-By: svsns777 <74819520+svsns777@users.noreply.github.com>

--- kiosk/__init__.py
+++ kiosk/__init__.py
@@ -1,6 +1,7 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
+from kiosk.db import get_db
 
 
 def create_app(test_config=None):
@@ -29,6 +30,18 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
+    @app.route('/manage_menu', methods=["POST", "GET"])
+    def add_menu():
+        db = get_db()
+        c = db.cursor()
+        c.execute('SELECT NAME, PRICE FROM MENU WHERE ID IN (SELECT MENU_ID FROM MENU_CATEGORY WHERE CATEGORY_TAG ="햄버거")'
+                   )
+        info = c.fetchall()
+        db.close()
+        return render_template('/manage_menu/add_menu.html', data=info)
+
+    from . import manage_menu
+    app.register_blueprint(manage_menu.bp)
     
     from . import db
     db.init_app(app)
@@ -36,7 +49,6 @@ def create_app(test_config=None):
     from . import order
     app.register_blueprint(order.bp)
     # app.add_url_rule('/', endpoint='index') # 필요한 코드??
-    
     
     return app
     
