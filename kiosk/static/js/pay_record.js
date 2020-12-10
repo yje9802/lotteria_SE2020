42fa93860e8a3cd67a4b2a23d3314e388405acc0
boram2445 <68495264+boram2445@users.noreply.github.com>
2020-12-10 16:53:02 +0900

    결제이력 - 환불/삭제 기능 추가
    환불/ 삭제 버튼 클릭-> 삭제를 원하는 이력을 표에서 선택하면 색상 변경 -> 경고 창 생성 -> 확인 버튼 클릭시 삭제
    (삭제 후에 페이지 재부팅 필요)

---
+++ kiosk/static/js/pay_record.js
@@ -0,0 +1,50 @@
+"use strict";
+
+
+const remove_btn = document.querySelector(".remove");
+const describe_remove = document.querySelector(".describe_remove");
+
+//환불/삭제 버튼 클릭
+function clickRemoveBtn(){
+    remove_btn.addEventListener("click",()=>{
+        remove_btn.classList.add("clicked");
+        describe_remove.style.display = "block";
+    })
+}
+
+
+//클릭된 행 색상 변경
+var orgBColor = '#ffffff';
+var orgTColor = '#000000';
+function HighLightTR(target, backColor,textColor) {
+    if (remove_btn.classList.contains("clicked")){
+        var tbody = target.parentNode;
+        var trs = tbody.getElementsByTagName('tr');
+            for ( var i = 0; i < trs.length; i++ ) {
+                if ( trs[i] != target ) {
+                trs[i].style.backgroundColor = orgBColor;
+                trs[i].style.color = orgTColor;
+            } else {
+                trs[i].style.backgroundColor = backColor;
+                trs[i].style.color = textColor;
+                setTimeout(function() {
+                    func_confirm();
+                    }, 100);
+                } 
+            }} 
+    } 
+
+function func_confirm(){
+    var do_remove;
+    do_remove=confirm("해당 내용을 삭제하시겠습니까?")
+    if(do_remove){
+        alert("삭제되었습니다");
+        remove_btn.classList.remove("clicked");
+    } else{
+        remove_btn.classList.remove("clicked");
+    }
+}
+
+
+
+clickRemoveBtn();
