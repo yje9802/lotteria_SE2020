0ae4989b93154bf230849d280c34e841b0a67d61
svsns777 <74819520+svsns777@users.noreply.github.com>
2020-12-10 23:43:15 +0900

    Update manage_menu_delete.py
    - 삭제 후 결과 표시
    - 이전에 선택했던 카테고리의 메뉴가 다른 작업시에도 변하지 않고 계속 보여지도록 수정

--- kiosk/manage_menu_delete.py
+++ kiosk/manage_menu_delete.py
@@ -12,50 +12,56 @@ from kiosk.db import get_db
 
 bp = Blueprint('delete_menu', __name__, url_prefix='/delete_menu')
 
+def set_query(n):
+    if n == 1:
+        return 'SELECT NAME, PRICE FROM MENU WHERE ID IN (SELECT MENU_ID FROM MENU_CATEGORY WHERE CATEGORY_TAG ="햄버거")'
+    elif n == 2:
+        return 'SELECT NAME, PRICE FROM MENU WHERE ID IN (SELECT MENU_ID FROM MENU_CATEGORY WHERE CATEGORY_TAG ="디저트" OR CATEGORY_TAG="치킨")'
+    elif n == 3:
+        return 'SELECT NAME, PRICE FROM MENU WHERE ID IN (SELECT MENU_ID FROM MENU_CATEGORY WHERE CATEGORY_TAG ="음료"  OR CATEGORY_TAG="커피")'
 
 @bp.route('/', methods=['GET'])
 def view_menu():
     db = get_db()
     c = db.cursor()
-    category_tag='햄버거'
-    c.execute('SELECT NAME, PRICE FROM MENU WHERE ID IN (SELECT MENU_ID FROM MENU_CATEGORY WHERE CATEGORY_TAG =?)', (category_tag,)
-              )
+
+    global category_tag    
+    category_tag=1
+    query = set_query(category_tag)
+    c.execute(query)
     info = c.fetchall()
+    
     if request.method == 'GET':
         if request.args.get('burger'):
-            c.execute('SELECT NAME, PRICE FROM MENU WHERE ID IN (SELECT MENU_ID FROM MENU_CATEGORY WHERE CATEGORY_TAG ="햄버거")'
-                   )
+            category_tag=1
+            c.execute(query )
             info = c.fetchall()
             db.close()
-            return render_template('/manage_menu/delete_menu.html', data=info, menu_data=0)
+            return render_template('/manage_menu/delete_menu.html', data=info, menu_data=0, name=0)
         elif request.args.get('dessert'):
-            c.execute('SELECT NAME, PRICE FROM MENU WHERE ID IN (SELECT MENU_ID FROM MENU_CATEGORY WHERE CATEGORY_TAG ="디저트" OR CATEGORY_TAG="치킨")'
-                   )
+            category_tag=2
+            query = set_query(category_tag)
+            c.execute(query)
             info = c.fetchall()
             db.close()
-            return render_template('/manage_menu/delete_menu.html', data=info, menu_data=0)
+            return render_template('/manage_menu/delete_menu.html', data=info, menu_data=0, name=0)
         elif request.args.get('beverage'):
-            c.execute('SELECT NAME, PRICE FROM MENU WHERE ID IN (SELECT MENU_ID FROM MENU_CATEGORY WHERE CATEGORY_TAG ="음료"  OR CATEGORY_TAG="커피")'
-                  )
+            category_tag=3
+            query = set_query(category_tag)
+            c.execute(query)
             info = c.fetchall()
             db.close()
-            return render_template('/manage_menu/delete_menu.html', data=info, menu_data=0)
-        c.execute('SELECT NAME, PRICE FROM MENU WHERE ID IN (SELECT MENU_ID FROM MENU_CATEGORY WHERE CATEGORY_TAG =?)', (category_tag,)
-              )
-        info = c.fetchall()
-        db.close()
-        return render_template('/manage_menu/delete_menu.html', data=info, menu_data=0)
+            return render_template('/manage_menu/delete_menu.html', data=info, menu_data=0, name=0)
     db.close()
-    return render_template('/manage_menu/delete_menu.html', data=info, menu_data=0)
+    return render_template('/manage_menu/delete_menu.html', data=info, menu_data=0, name=0)
 
 
 @bp.route('/detail', methods=['POST'])
 def view_detail():
     db = get_db()
     c = db.cursor()
-    category_tag='햄버거'
-    c.execute('SELECT NAME, PRICE FROM MENU WHERE ID IN (SELECT MENU_ID FROM MENU_CATEGORY WHERE CATEGORY_TAG =?)', (category_tag,)
-              )
+    query=set_query(category_tag)
+    c.execute(query)
     info = c.fetchall()
     if request.method=='POST':
         menu_name=request.form['view_name']
@@ -68,9 +74,8 @@ def view_detail():
 def delete_menu():
     db = get_db()
     c = db.cursor()
-    category_tag='햄버거'
-    c.execute('SELECT NAME, PRICE FROM MENU WHERE ID IN (SELECT MENU_ID FROM MENU_CATEGORY WHERE CATEGORY_TAG =?)', (category_tag,)
-              )
+    query=set_query(category_tag)
+    c.execute(query)
     info = c.fetchall()
     if request.method=='POST':
         menu_name=request.form['name']
@@ -83,5 +88,17 @@ def delete_menu():
             'DELETE FROM "MENU" WHERE NAME=?', (menu_name,))
         db.commit()
         db.close()
-        return redirect(url_for('delete_menu.view_menu'))
+
+        global name
+        name = menu_name
+        return redirect(url_for('delete_menu.result', name=name))
+
+@bp.route('/result')
+def result():
+    db = get_db()
+    c = db.cursor()
+    query=set_query(category_tag)
+    c.execute(query)
+    info = c.fetchall()
+    return render_template('/manage_menu/delete_menu.html', data=info, menu_data=0, name=name)
  
