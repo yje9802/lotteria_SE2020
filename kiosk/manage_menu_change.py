afb0ab4b24c8773bbb8c6089dd01a34c1fc7dbe4
svsns777 <74819520+svsns777@users.noreply.github.com>
2020-12-10 23:41:28 +0900

    Update manage_menu_change.py
    - 수정 후 결과 표시
    - 결과를 표시거나 메뉴를 불러오는 등 다른 작업을 할 때에도 기존에 선택했던 카테고리의 메뉴 목록을 보여줄 수 있도록 수정

--- kiosk/manage_menu_change.py
+++ kiosk/manage_menu_change.py
@@ -12,41 +12,41 @@ from kiosk.db import get_db
 
 bp = Blueprint('change_menu', __name__, url_prefix='/change_menu')
 
+def set_query(n):
+    if n == 1:
+        return 'SELECT NAME, PRICE FROM MENU WHERE ID IN (SELECT MENU_ID FROM MENU_CATEGORY WHERE CATEGORY_TAG ="햄버거")'
+    elif n == 2:
+        return 'SELECT NAME, PRICE FROM MENU WHERE ID IN (SELECT MENU_ID FROM MENU_CATEGORY WHERE CATEGORY_TAG ="디저트" OR CATEGORY_TAG="치킨")'
+    elif n == 3:
+        return 'SELECT NAME, PRICE FROM MENU WHERE ID IN (SELECT MENU_ID FROM MENU_CATEGORY WHERE CATEGORY_TAG ="음료"  OR CATEGORY_TAG="커피")'
 
 @bp.route('/', methods=['GET'])
 def view_menu():
     db = get_db()
     c = db.cursor()
-    category_tag='햄버거'
-    c.execute('SELECT NAME, PRICE FROM MENU WHERE ID IN (SELECT MENU_ID FROM MENU_CATEGORY WHERE CATEGORY_TAG =?)', (category_tag,)
-              )
+    
+    global category_tag    
+    category_tag=1
+    query = set_query(category_tag)
+    c.execute(query)
     info = c.fetchall()
     if request.method == 'GET':
         if request.args.get('burger'):
-            c.execute('SELECT NAME, PRICE FROM MENU WHERE ID IN (SELECT MENU_ID FROM MENU_CATEGORY WHERE CATEGORY_TAG ="햄버거")'
-                   )
+            category_tag=1
+            c.execute(query )
             info = c.fetchall()
-            db.close()
-            return render_template('/manage_menu/change_menu.html', data=info, menu_data=0)
         elif request.args.get('dessert'):
-            c.execute('SELECT NAME, PRICE FROM MENU WHERE ID IN (SELECT MENU_ID FROM MENU_CATEGORY WHERE CATEGORY_TAG ="디저트" OR CATEGORY_TAG="치킨")'
-                   )
+            category_tag=2
+            query = set_query(category_tag)
+            c.execute(query)
             info = c.fetchall()
-            db.close()
-            return render_template('/manage_menu/change_menu.html', data=info, menu_data=0)
         elif request.args.get('beverage'):
-            c.execute('SELECT NAME, PRICE FROM MENU WHERE ID IN (SELECT MENU_ID FROM MENU_CATEGORY WHERE CATEGORY_TAG ="음료"  OR CATEGORY_TAG="커피")'
-                  )
+            category_tag=3
+            query = set_query(category_tag)
+            c.execute(query)
             info = c.fetchall()
-            db.close()
-            return render_template('/manage_menu/change_menu.html', data=info, menu_data=0)
-        c.execute('SELECT NAME, PRICE FROM MENU WHERE ID IN (SELECT MENU_ID FROM MENU_CATEGORY WHERE CATEGORY_TAG =?)', (category_tag,)
-              )
-        info = c.fetchall()
-        db.close()
-        return render_template('/manage_menu/change_menu.html', data=info, menu_data=0)
     db.close()
-    return render_template('/manage_menu/change_menu.html', data=info, menu_data=0)
+    return render_template('/manage_menu/change_menu.html', data=info, menu_data=0, name=0)
 
 
 @bp.route('/detail', methods=['POST'])
@@ -59,18 +59,19 @@ def view_detail():
     info = c.fetchall()
     if request.method=='POST':
         menu_name=request.form['view_name']
+        global original_name
+        original_name = menu_name
         c.execute('SELECT NAME, IMAGE_PATH, PRICE, DESC, WEIGHT_G, KCAL, PROTEIN_G, PROTEIN_PCENT, SODIUM_MG, SODIUM_PCENT, SUGAR_G, SAT_FAT_G, SAT_FAT_PCENT, CAFFEINE_MG, ALLERGY_INFO FROM MENU WHERE NAME=?',(menu_name,))
         menu_data=c.fetchall()
         db.close()
-        return render_template('/manage_menu/change_menu.html', data=info, menu_data=menu_data)
+        return render_template('/manage_menu/change_menu.html', data=info, menu_data=menu_data, name=0)
 
 @bp.route('/change', methods=['POST'])
 def change_menu():
     db = get_db()
     c = db.cursor()
-    category_tag='햄버거'
-    c.execute('SELECT NAME, PRICE FROM MENU WHERE ID IN (SELECT MENU_ID FROM MENU_CATEGORY WHERE CATEGORY_TAG =?)', (category_tag,)
-              )
+    query=set_query(category_tag)
+    c.execute(query)
     info = c.fetchall()
     if request.method=='POST':
         menu_name = request.form['name']
@@ -89,12 +90,27 @@ def change_menu():
         menu_caff = float(request.form['caff'])
         menu_allergy = request.form['allergy']
         
-        c.execute('SELECT ID FROM MENU WHERE NAME=?', (menu_name,))
+        c.execute('SELECT ID FROM MENU WHERE NAME=?', (original_name,))
         tmp = c.fetchone()
         menu_id=tmp[0]
         db.execute('UPDATE MENU SET NAME=?, IMAGE_PATH=?, PRICE=?, DESC=?, WEIGHT_G=?, KCAL=?, PROTEIN_G=?, PROTEIN_PCENT=?, SODIUM_MG=?, SODIUM_PCENT=?, SUGAR_G=?, SAT_FAT_G=?, SAT_FAT_PCENT=?, CAFFEINE_MG=?, ALLERGY_INFO=? WHERE ID=?',(menu_name, menu_image, menu_price, menu_desc, menu_weight, menu_kcal, menu_protein_g, menu_protein_pct, menu_sodium_g, menu_sodium_pct, menu_sugar, menu_satfat_g, menu_satfat_pct, menu_caff, menu_allergy, menu_id)
                    )
         db.commit()
         db.close()
-        return redirect(url_for('change_menu.view_menu'))
- 
+
+        global names
+        names=[]                                        #변경 전 후 메뉴명 리스트
+        names.append(original_name)
+        names.append(menu_name)
+        return redirect(url_for('change_menu.result', names=names))
+
+@bp.route('/result')
+def result():
+    db = get_db()
+    c = db.cursor()
+    query=set_query(category_tag)
+    c.execute(query)
+    info = c.fetchall()
+    return render_template('/manage_menu/change_menu.html', data=info, menu_data=0, name=names)
+                           
+    
