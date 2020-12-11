0f5ac308910bedb0a40d74e94d36b78421f55c1a
boram2445 <68495264+boram2445@users.noreply.github.com>
2020-12-11 15:54:30 +0900

    환불버튼 수정 추가
    (취소 누르면 원래 화면으로)

--- kiosk/static/js/pay_record.js
+++ kiosk/static/js/pay_record.js
@@ -13,7 +13,7 @@ function clickRemoveBtn(){
 }
 
 
-//클릭된 행 색상 변경 (삭제 후에 적용되지 않도록 재부팅(?) 필요할듯)
+//클릭된 행 색상 변경
 var orgBColor = '#ffffff';
 var orgTColor = '#000000';
 function HighLightTR(target, backColor,textColor) {
@@ -34,7 +34,7 @@ function HighLightTR(target, backColor,textColor) {
             }} 
     } 
 
-//환불/삭제 후 경고창
+//환불/삭제 후 경고창 (삭제 경고창 '확인'선택 후에도 색상이 지워지지 않아서 재부팅(?)필요할듯..)
 function func_confirm(){
     var do_remove;
     do_remove=confirm("해당 내용을 삭제하시겠습니까?")
@@ -42,10 +42,7 @@ function func_confirm(){
         alert("삭제되었습니다");
         remove_btn.classList.remove("clicked");
         describe_remove.style.display = "none";
-    } else{
-        //remove_btn.classList.remove("clicked");
-        describe_remove.style.display = "none";
-    }
+    } 
 }
 
 
