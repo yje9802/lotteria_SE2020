0980e63a9665c1f522b7d8936bdfdfc86df15676
svsns777 <74819520+svsns777@users.noreply.github.com>
2020-12-14 17:42:48 +0900

    manager_btn 경로 수정

--- kiosk/static/js/auth.js
+++ kiosk/static/js/auth.js
@@ -8,7 +8,7 @@ function login(username, password){
 	id += "";
 	pwd += "";
 	if ((id == username) && (pwd == password)){
-		return window.location.href='{{ url_for('auth.manager') }}';
+		return window.location.href='{{ url_for('auth.manager_btn') }}';
 	}
 	else{
 		alert('로그인 실패');
